37dc8fa00a9a6586a0e41569c2d2a3cb3a2ec540
Manish Bisht <manish.bisht490@gmail.com>
2021-04-22 12:00:58 +0530

    project init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GoogleSheetsProvider from 'react-db-google-sheets';
+import Home from './pages/home';
+
+function App() {
+  return (
+      <GoogleSheetsProvider>
+        <Home />
+      </GoogleSheetsProvider>
+  );
+}
+
+export default App;
